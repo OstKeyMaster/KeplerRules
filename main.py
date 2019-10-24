25d3ab2474dd79cce1e1a05afbb379c9ca51a35c
OstKeyMaster <ostkeymaster@yandex.ru>
2019-10-24 23:10:50 +0300

    start point

--- main.py
+++ main.py
@@ -11,5 +11,15 @@ y = y0
 h = float(input())
 ax = -G * M * x / ((x ** 2 + y ** 2) ** (3 / 2))
 fi = (f(h) - f(h / 2)) / f(h)
-print(fi)
-print(f(h/2))
+print('rel error =', fi)
+
+p0 = v0x
+p = p0 + h*ax
+x = x + h*(p + p0)/2
+print('x =', x)
+
+ay = -G * M * y / ((x ** 2 + y ** 2) ** (3 / 2))
+k0 = v0y
+k = k0 + h*ay
+y = y + h*(k + k0)/2
+print('y =', y)
